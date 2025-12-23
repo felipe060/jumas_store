5c921d6df4d1800ff4c68729885cc1c3dd990878
felipe060 <felipeflip29@hotmail.com>
2025-12-23 14:44:14 -0300

    9--> added SessionCode class to conn.py

--- back/conn.py
+++ back/conn.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Date, DateTime, Float, text, Boolean, sql, update, func
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship, query
 from dotenv import find_dotenv, load_dotenv
 from os import getenv, environ
+from datetime import datetime as datetime_, timedelta
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
@@ -28,8 +29,71 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, nullable=False, default=func.now())
 
+    user_sessioncode = relationship("SessionCode", back_populates="id_usuario")
 
-Base.metadata.create_all(engine)
+
+class SessionCode(Base):
+    __tablename__ = "tb_sessioncode"
+
+    sessioncode_id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    user_id = Column(Integer, ForeignKey("tb_users.user_id"), nullable=False)
+    sessioncode = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=str(datetime_.now())[:-7])
+    expires_at = Column(DateTime, nullable=False, default=str(datetime_.now()+timedelta(minutes=10))[:-7])
+
+    id_usuario = relationship("User", back_populates="user_sessioncode")
+
+
+class Address(Base):
+    __tablename__ = "tb_addresses"
+
+    address_id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+
+
+#Base.metadata.create_all(engine)
+
+
+
+
+"""with Session() as session:
+    novo = SessionCode(user_id=2, sessioncode="aq_o_sessioncode")
+    session.add(novo)
+    session.commit()
+    #sqlalchemy.exc.IntegrityError: (psycopg2.errors.ForeignKeyViolation) insert or update on table "tb_sessioncode" violates foreign key constraint "tb_sessioncode_user_id_fkey"
+    """
+
+
+"""with Session() as session:
+    consulta = session.query(User.user_email).where(User.user_id == 2).first()
+    #consulta = session.query(User).where(User.user_id == 2).first()
+    print("")
+    print(consulta)
+    print(consulta.user_email)
+    ala = consulta.user_email
+    #print(consulta.is_active)"""
+
+
+
+
+"""adicionar = User(user_email="sofia@hotmail.com", user_senha="senha_sofia", name="sofia")
+with Session() as session:
+    session.add(adicionar)
+    session.commit()
+
+    #sqlalchemy.exc.IntegrityError: (psycopg2.errors.UniqueViolation) duplicate key value violates unique constraint "tb_users_user_email_key"""
+
+
+
+
+
+
+
+
+"""ola = datetime_.now()
+print(ola)
+oi = ola + timedelta(minutes=200)
+print("--> ", oi)"""
+
 
 
 """class User(Base):
@@ -56,3 +120,21 @@ class SessionCode(Base):
 
 
 
+
+"""hora_agr = datetime_.now()
+futuro = hora_agr + timedelta(minutes=1)
+print("hora agr--> ", hora_agr)
+print("futuro --> ", futuro)
+
+seila = "2025-12-23 13:28:16"
+format_string = '%Y-%m-%d %H:%M:%S'
+
+datetime_object = datetime_.strptime(seila, format_string)
+
+print("--> ", datetime_object)
+print("type --> ", type(datetime_object))
+
+if futuro > hora_agr:
+    print("seila")
+else:
+    print("oloco")"""
