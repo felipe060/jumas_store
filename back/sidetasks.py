9bc20ce3e49e59916231e11ece2ac2012917b264
felipe060 <felipeflip29@hotmail.com>
2026-01-03 08:45:15 -0300

    21--> more code on sidetasks.py

--- back/sidetasks.py
+++ back/sidetasks.py
@@ -1,12 +1,12 @@
 def verify_header(**kwargs):
-    content_type = kwargs["content_type"]
+    content_type = kwargs["content_type"]       #recebi o content-type atraves do kwargs com type string
 
-    if content_type.strip() == "":
-        message = {"error": "missing //content-type// header"}
+    if content_type.strip() == "":                                  #verifies if the content-type is indeed in the request
+        message = {"error": "missing //content-type// header"}      #if not, returns a json
         return message
 
-    elif content_type != "application/json":
-        message = {"error": "//content-type// must be application/json"}
+    elif content_type != "application/json":                                    #verifies if the header id content-type=application/json
+        message = {"error": "//content-type// must be application/json"}        #if not, return a json
         return message
 
     elif content_type == "application/json":
@@ -18,7 +18,7 @@ def verifies_user(**kwargs):
 
     print("sidetasks.py verifies_user() being called\n")
 
-    json_data = kwargs["json_data"]                         #receives the json from the app.py and put it into a variable
+    json_data = kwargs["json_data"]             #receives the json from the app.py and put it into a variable
 
     json_data_keys = json_data.keys()           #put the json keys on a variable
     lista: list = []
@@ -28,8 +28,8 @@ def verifies_user(**kwargs):
     len_lista = len(lista)                      #checks the length of the list with json keys
 
     if len_lista != 2:                                                          #if the list has something different from 2 items, its over pro betinha
-        print("o json recebido tem um numero de campos diferente de 2")         #eu so preciso de 2 itens //email// e //senha//
-        message: dict = {"error": "the json file received has a number of fields differente from 2"}
+        print("o json recebido tem um numero de campos diferente de 2\n")       #eu so preciso de 2 itens //email// e //senha//
+        message: dict = {"error": "the json file received has a number of fields different from 2"}
         return message
 
     elif len_lista == 2:            #if the json received has exactly 2 items
@@ -44,13 +44,15 @@ def verifies_user(**kwargs):
     email = json_data["email"]
     senha = json_data["senha"]
 
-    try:
+    try:                #se o json tem apenas 2 campos e eles sao //email// e //senha//, ent seguimos com esse try
         from database_conn import verify_on_database
         result_user_query = verify_on_database(email=email, senha=senha)
         return result_user_query
 
     except Exception as e:
-        print("eita\n"
+        print("eita, surgiu uma Exception\n"
               "-- love the Lord your God with all your heart and with all you soul and with all your mind")
         print(e)
         return "unknown exception"
+
+
